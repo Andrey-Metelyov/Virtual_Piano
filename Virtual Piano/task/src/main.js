64c1b727af91eb7fdbc17127639308a65bf301d2
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-03-21 12:18:52 +0900

    4/5 done

--- Virtual Piano/task/src/main.js
+++ Virtual Piano/task/src/main.js
@@ -1,14 +1,39 @@
+
+
 document.addEventListener("keydown", function(event) {
-    if (event.code === "KeyA" ||
-        event.code === "KeyS" ||
-        event.code === "KeyD" ||
-        event.code === "KeyF" ||
-        event.code === "KeyG" ||
-        event.code === "KeyH" ||
-        event.code === "KeyJ"
-    ) {
-        console.log(`The '${event.key}' key is pressed.`);
-    } else {
-        console.log("Warning: wrong key");
+    let aud = document.createElement("AUDIO");
+    switch (event.code) {
+        case "KeyA":
+            // A.play();
+            aud.setAttribute("src", "aud/A.mp3");
+            break;
+        case "KeyS":
+            // S.play();
+            aud.setAttribute("src", "aud/S.mp3");
+            break;
+        case "KeyD":
+            // D.play();
+            aud.setAttribute("src", "aud/D.mp3");
+            break;
+        case "KeyF":
+            // F.play();
+            aud.setAttribute("src", "aud/F.mp3");
+            break;
+        case "KeyG":
+            // G.play();
+            aud.setAttribute("src", "aud/G.mp3");
+            break;
+        case "KeyH":
+            // H.play();
+            aud.setAttribute("src", "aud/H.mp3");
+            break;
+        case "KeyJ":
+            // J.play();
+            aud.setAttribute("src", "aud/J.mp3");
+            break;
+        default:
+            console.log("Warning: wrong key");
+            return;
     }
+    aud.play();
 });
